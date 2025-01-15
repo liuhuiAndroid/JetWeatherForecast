64c2101d2ceb52c6af93f63e1f5aa941304db654
lh <im.lh@hotmail.com>
2025-01-15 14:22:22 +0800

    test

--- app/src/main/java/com/weather/forecast/MainActivity.kt
+++ app/src/main/java/com/weather/forecast/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.weather.forecast
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -29,6 +30,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             WeatherApp()
         }
+        startActivity(Intent(this, FlexboxLayoutTestActivity::class.java))
     }
 }
 
